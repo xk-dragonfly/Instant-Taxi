796788f8506f8276c996afbb34c49a15e4c960a1
xk-dragonfly <xk0708666@gmail.com>
2024-08-17 20:12:13 +0800

    inital gateway

---
+++ server-gateway/src/main/java/com/xk/servergateaway/ServerGateawayApplication.java
@@ -0,0 +1,13 @@
+package com.xk.servergateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGateawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGateawayApplication.class, args);
+    }
+
+}
---
+++ server-gateway/src/test/java/com/xk/servergateaway/ServerGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xk.servergateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGateawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
